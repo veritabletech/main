b5909cc437e1ff691544f5abadc79bc633495a3a
ikilikao <63195974+ikilikao@users.noreply.github.com>
2020-04-25 11:42:06 +0200

    Amélioration du script paiement.py

--- paiement.py
+++ paiement.py
@@ -1,17 +1,41 @@
-import bitsv
+private_key = None
+
+#Import préliminaires
 import sys
-import random
 import os
+import random
+
+#Si l'utilisateur veut repartir de zéro on mets premier_lancement à True
+if len(sys.argv) > 1 and sys.argv[1] == "reset":
+    premier_lancement = True
+
+#On demande la clé privée puis on quitte si l'utilisateur voulait la changer
+if not private_key:
+    print("Ce script a besoin d'une clé privée pour fonctionner")
+    clef = input("Votre clé > ")
+    nom = os.path.basename(__file__)
+    nom_tmp = str(random.random())
+    os.system("echo 'private_key = \"" + clef + "\"' > " + nom_tmp)
+    os.system("tail --lines=+2 " + nom + " >> " + nom_tmp)
+    os.system("mv " + nom_tmp + " " + nom)
+    print("Votre fichier a bien été configuré avec votre clé privée")
+    exit()
+
+#On vérifie que l'utilisateur a compris comment marchait le script
+if len(sys.argv) < 5:
+    print("Utilisation: python3 script.py identifiant artiste oeuvre tirage")
+    exit()
+
+# Execution du script
+import bitsv
+my_key = bitsv.Key(private_key)
+
 import urllib3
+http = urllib3.PoolManager()
+
 import hashlib, binascii
 import string
 import secrets
-
-http = urllib3.PoolManager()
-
-public_key = "18sRMTTFXJ1MK13D8LVN8poFCEn1nBbuAS"
-private_key = ""
-my_key = bitsv.Key(private_key)
 
 #On parse l'entrée
 identifiant = sys.argv[1]
@@ -22,6 +46,7 @@ tirage = sys.argv[4]
 #On génère le mot de passe
 alphabet = string.ascii_uppercase.replace("O", "") + string.digits
 mot_de_passe = ''.join(secrets.choice(alphabet) for i in range(20))
+
 #On affiche par exemple le mot de passe
 print(mot_de_passe, end="")
 
